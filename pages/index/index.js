2be0552e5a3dda656f58f8253bc32efd84d180a2
elkin.silva <elkinsilvamanrique@gmail.com>
2023-03-27 16:02:31 -0500

    first commit

---
+++ pages/index/index.js
@@ -0,0 +1,27 @@
+  Page({
+    data: {
+      board: [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""]
+      ],
+      currentPlayer: "X"
+        },
+    
+
+    handleCellClick: function(event) {
+      console.log("click")
+      var row = event.currentTarget.dataset.row;
+      var col = event.currentTarget.dataset.col;
+      var board = this.data.board;
+      var currentPlayer = this.data.currentPlayer;
+    
+      if (board[row][col] == '') {
+        board[row][col] = currentPlayer;
+        this.setData({
+          board: board,
+          currentPlayer: currentPlayer == 'X' ? 'O' : 'X'
+        });
+      }
+    }
+  });
